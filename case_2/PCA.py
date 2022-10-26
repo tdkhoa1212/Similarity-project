6ebf4752ea5fdfaa79a752df38ab5db2e25edfbf
tdkhoa1212 <tdkhoa1212@gmail.com>
2022-10-26 17:33:35 +0700

    new

--- case_2/PCA.py
+++ case_2/PCA.py
@@ -2,30 +2,29 @@ import numpy as np
 from sklearn.decomposition import PCA
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
+from ssqueezepy import ssq_cwt
+from scipy import signal
 
-def reshape_input(x, nuts=4):
+def spectrogram(filename):
     '''
-    reshape vectors to matrices 
-    with the shape of (nuts, ...)
+    convert raw audio to spectrogram
     '''
-    if x.shape[0]%nuts == 0:
-        x_new = x.reshape(nuts, -1)
-    else:
-        o_length = x.shape[0]
-        n_dimension = int(o_length - (o_length%nuts))
-        x_new = x.reshape(nuts, n_dimension)
-    return x_new 
-
+    sample_rate, x = wavfile.read(filename) 
+    Twx, Wx, *_ = ssq_cwt(x) 
+    f, t, Sxx = signal.spectrogram(x, sample_rate)  
+    x_out = abs(Sxx)
+    # a, b = x_out.shape 
+    # print(x_out.shape)
+    # x_out = x_out.reshape(b, a)
+    return x_out
     
 def PCA_transform(filename):
-    # get a vector from an audio file
-    sample_rate, x = wavfile.read(filename) 
-
     # transform the vector to a matric
-    x_new = reshape_input(x)
+    x = spectrogram(filename)
+    print(f'Spectrogram (Twx)shape: {x.shape}\n')
 
     # reduce dimension of matric by PCA
     pca = PCA(n_components=1, svd_solver='arpack')
-    y = np.squeeze(pca.fit_transform(x_new))
+    y = np.squeeze(pca.fit_transform(x))
     return y
     
