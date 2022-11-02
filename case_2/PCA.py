adcbde82f0baedc1ee8e24aff727a84a41c4ff56
tdkhoa1212 <tdkhoa1212@gmail.com>
2022-11-02 12:21:42 +0700

    new

--- case_2/PCA.py
+++ case_2/PCA.py
@@ -2,26 +2,26 @@ import numpy as np
 from sklearn.decomposition import PCA
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
-from ssqueezepy import ssq_cwt
-from scipy import signal
+import librosa
 
 def spectrogram(filename):
     '''
-    convert raw audio to spectrogram
+    convert raw audio to spectrogram using the constant-Q transform
+    source: https://librosa.org/doc/main/generated/librosa.cqt.html
     '''
-    sample_rate, x = wavfile.read(filename) 
-    Twx, Wx, *_ = ssq_cwt(x) 
-    x_out = abs(Wx)
-    print(x_out.shape)
-    return x_out
+    # sample_rate, x = wavfile.read(filename) 
+    x, sample_rate = librosa.load(filename)
+    C = np.abs(librosa.cqt(x, sr=sample_rate, n_bins=60))
+    return C, sample_rate
     
 def PCA_transform(filename):
     # transform the vector to a matric
-    x = spectrogram(filename)
-    print(f'Spectrogram (Twx)shape: {x.shape}\n')
+    C, sample_rate = spectrogram(filename)
+    print(f'\nSpectrogram C shape: {C.shape} in (frequency, time)')
 
     # reduce dimension of matric by PCA
     pca = PCA(n_components=1, svd_solver='arpack')
-    y = np.squeeze(pca.fit_transform(x))
+    y = np.squeeze(pca.fit_transform(C))
+    print(f'PCA shape: {y.shape}\n')
     return y
     
--- case_2/plot_extraction.py
+++ case_2/plot_extraction.py
@@ -1,20 +1,31 @@
 import matplotlib.pyplot as plt
 import click
-from PCA import PCA_transform
+import numpy as np
+import librosa.display
+from PCA import spectrogram, PCA_transform
 
 @click.command()
 # ******************************Enter your expected information *****************************
-@click.option('--filename1', default='/home/ubuntu-pc/Enrico_boss/Similarity-project/wav/Matthew_Male/lemon_and_apple.wav', help='direction of .wav input file')
-@click.option('--filename2', default='/home/ubuntu-pc/Enrico_boss/Similarity-project/wav/Matthew_Male/orange_and_lemon.wav', help='direction of .wav input file')
-@click.option('--savefile', default='/home/ubuntu-pc/Enrico_boss/Similarity-project/images/Wx_same.png', help='direction of .wav output file')
-def plot(filename1, filename2, savefile):
-    pca1 = PCA_transform(filename1)
-    pca2 = PCA_transform(filename2)
-    plt.plot(pca1, label=filename1.split('/')[-1])
-    plt.plot(pca2, label=filename2.split('/')[-1])
-    plt.legend()
-    plt.savefig(savefile)
-    plt.show()
+@click.option('--filename', default='/home/ubuntu-pc/Enrico_boss/Similarity-project/wav/440hz.wav', help='direction of .wav input file')
+@click.option('--savefile', default='/home/ubuntu-pc/Enrico_boss/Similarity-project/images/440hz.png', help='direction of .wav output file')
+def plot(filename, savefile):
+    # Spectrogram and PCA outputs--------------------------
+    C, sample_rate = spectrogram(filename)
+    y = PCA_transform(filename)
+    print(max(y))
+
+    # Get name---------------------------------------------
+    name = filename.split('/')[-1].split('.')[0]
+
+    # plot outputs-----------------------------------------
+    fig, ax = plt.subplots(2)
+    img = librosa.display.specshow(C, sr=sample_rate, x_axis='time', y_axis='cqt_hz', ax=ax[0])
+    ax[0].set_title(f'Constant-Q power spectrum of {name}')
+    ax[1].plot(y)
+    ax[1].set(xlabel='Frequency resolution', ylabel='Amplitude')
+    fig.colorbar(img, ax=ax[0], format="%+2.0f dB")
+    fig.savefig(savefile)
+
 
 if __name__ == '__main__':
     plot()
